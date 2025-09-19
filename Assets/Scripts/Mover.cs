b71b494565309606dc17ae4ba229faddd0ea50a0
Vitaliis <Vitalik12-14@mail.ru>
2025-09-19 21:18:02 +0300

    Add running

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,10 +5,17 @@ public class Mover : MonoBehaviour
 {
     private const string Horizontal = nameof(Horizontal);
 
+    [SerializeField] private TriggerHandler _triggerHandler;
+
     [SerializeField, Min(0)] private float _speed;
+    [SerializeField, Min(0)] private float _runningSpeed;
 
     private Rigidbody2D _rigidbody;
+
     private float _direction;
+
+    private bool _isRunning;
+    private bool _isGrounded;
 
     private void Awake()
     {
@@ -16,12 +23,33 @@ public class Mover : MonoBehaviour
         _rigidbody.freezeRotation = true;
     }
 
+    private void OnEnable()
+        => _triggerHandler.Triggered += SetGrounded;
+
+    private void OnDisable()
+        => _triggerHandler.Triggered -= SetGrounded;
+
     private void Update()
-        => _direction = Input.GetAxis(Horizontal);
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+            _isRunning = true;
+
+        _direction = Input.GetAxis(Horizontal);
+    }
 
     private void FixedUpdate()
     {
-        _direction = _direction * _speed * Time.fixedDeltaTime;
+        _direction *= _speed * Time.fixedDeltaTime;
+
+        if(_isRunning && _isGrounded)
+        {
+            _direction *= _runningSpeed;
+            _isRunning = false;
+        }
+
         _rigidbody.velocity = new(_direction, _rigidbody.velocity.y);
     }
+
+    private void SetGrounded(bool isGrounded)
+        => _isGrounded = isGrounded;
 }
