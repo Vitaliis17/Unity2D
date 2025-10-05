d8e0237f5882dbdce692119566ffdb55cee6f78d
Vitaliis <Vitalik12-14@mail.ru>
2025-10-05 13:03:24 +0300

    Complete 'Битва за платформер - Начало'(1)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,32 +2,37 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField, Min(0)] private float _maxHealth;
+    [SerializeField, Min(0)] private int _maxHealthAmount;
 
-    private float _currentHealth;
+    private int _currentHealthAmount;
 
     private void Awake()
-        => _currentHealth = _maxHealth;
+        => _currentHealthAmount = _maxHealthAmount;
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
         if (damage <= 0)
             return;
 
-        _currentHealth -= damage;
+        _currentHealthAmount -= damage;
 
         if(IsDead())
             Destroy(gameObject);
     }
 
-    public void Heal(float healingAmount)
+    public void Heal(int healingAmount)
     {
         if(healingAmount <= 0) 
             return;
 
-        _currentHealth += healingAmount;
+        int nextHealthAmount = _currentHealthAmount + healingAmount;
+
+        if(nextHealthAmount > _maxHealthAmount)
+            nextHealthAmount = _maxHealthAmount;
+
+        _currentHealthAmount = nextHealthAmount;
     }
 
     private bool IsDead()
-        => _currentHealth <= 0;
+        => _currentHealthAmount <= 0;
 }
